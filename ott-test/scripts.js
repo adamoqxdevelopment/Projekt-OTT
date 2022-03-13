0095c9c60ca288f79e4efae01bae2e0b72398cd6
adam.wisniewski0905@gmail.com <adamoqx.kontakt@gmail.com>
2022-03-13 11:14:39 +0100

    Login panel update
    added login screen

---
+++ ott-test/scripts.js
@@ -0,0 +1,6 @@
+// var bg_opacity;
+// var obj_seria3 = document.getElementById("seria3");
+// var obj_root = document.querySelector(':root');
+// console.log(obj_seria3);
+// seria3.addEventListener("hover", obj_root.style.setProperty('--serie-opacity', '0.1'));
+// alert("wykonano");
